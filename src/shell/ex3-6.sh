6f499a5351d350c2dec4ab63ad2389dfa55adb1c
선영 <141916888+bunny-97@users.noreply.github.com>
2024-11-08 04:10:37 +0000

    Create ex3-6.sh

---
+++ src/shell/ex3-6.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -d "$1" ]
+then
+   :
+else
+   mkdir $1
+   cd $1
+   i=0
+   while [ $i -lt 5 ]
+   do
+       touch "file$i.txt"
+       i=$((i + 1))
+   done
+
+   tar cf $1.tar file0.txt file1.txt file2.txt file3.txt file4.txt
+   ls
+
+   mkdir $1
+
+   mv $1.tar $1
+   cd $1
+   tar xvf "$1.tar"
